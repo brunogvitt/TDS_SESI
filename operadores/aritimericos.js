4d5cfb848cbcbcf69823c6dff9812002c9897648
brunogvitt <brunoo.vitt.@gmail.com>
2026-03-05 12:12:45 -0300

    operadores aritmeticos

---
+++ operadores/aritimericos.js
@@ -0,0 +1,16 @@
+//VALORES LITERAIS
+console.log(10 + 5);// ADIÇÃO
+console.log(10 - 5);// SUBTRAÇÃO
+console.log(10 * 5);// MULTIPLICAÇÃO
+console.log(10 / 5);// DIVISÃO
+console.log(10 % 5);// RESTO DE DIVISÃO
+
+//VALORES VARIAVEIS
+let a = 100;
+let b = 50;
+
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
+console.log(a + b);
