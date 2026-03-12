9b73eafdbaa9fe6b4801472deb9f7c73c4d59d5f
brunogvitt <bruno.vitt@gmail.com>
2026-03-12 12:12:41 -0300

    ESTRUTURAS_de_CONTROLE

---
+++ switch.js
@@ -0,0 +1,26 @@
+let dia = 5;
+switch (dia){
+    case 1:
+        console.log("DOMINGO");
+        break;
+    case 2:
+        console.log("SEGUNDA-FEIRA");
+        break;
+    case 3:
+        console.log("TERÇA-FEIRA");
+        break;
+    case 4:
+        console.log("QUARTA-FEIRA");
+        break;
+    case 5:
+        console.log("QUINTA-FEIRA");
+        break;
+    case 6:
+        console.log("SEXTA-FEIRA");
+        break;
+    case 7:
+        console.log("SABADO");
+        break;
+    default:
+        console.log("NUMERO INVALIDO.");
+}
---
+++ template_string.js
@@ -0,0 +1,6 @@
+let nome ="Bruno";
+let idade = 16;
+// IMPRIMIR NO TERMINAL O NOME E A IDADE.
+// JUNTO COM UMA FRASE.
+console.log("Meu nome é" + nome +"e tenho" + idade + "anos de idade.");
+console,log(`Meu nome é ${nome} e tenho ${idade} anos de idade`);
